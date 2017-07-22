01b9c93c4d972c4d2750dae521386f7f4509ef12
Daniel Burgner <dburgnerjr@yahoo.com>
2017-07-22 00:52:42 -0400

    Main Menu setup
    Setup main menu buttons for both apps.

--- FlipulatorIOS/FlipulatorIOS/ViewController.swift
+++ FlipulatorIOS/FlipulatorIOS/ViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var btnAbout: UIButton!
+    @IBOutlet weak var btnCalculate: UIButton!
+    @IBOutlet weak var btnUpgradeDonate: UIButton!
+    @IBOutlet weak var btnDonateShare: UIButton!
+    @IBOutlet weak var btnShareOpenFiles: UIButton!
+    @IBOutlet weak var btnOpenFiles: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +25,30 @@ class ViewController: UIViewController {
         let paid = Bundle.main.object(forInfoDictionaryKey: "Paid")
         if (paid != nil) {
             label.text = "Flipulator Premium Version"
+            btnUpgradeDonate.setTitle("Donate", for: .normal)
+            btnDonateShare.setTitle("Share", for: .normal)
+            btnShareOpenFiles.setTitle("Open Files", for: .normal)
+            btnOpenFiles.isHidden = true
         } else {
             return
         }
     }
 
+    @IBAction func btnAbout(_ sender: Any) {
+    }
+    
+    @IBAction func btnCalculate(_ sender: Any) {
+    }
+    
+    @IBAction func btnUpgradeDonate(_ sender: Any) {
+    }
+    
+    @IBAction func btnDonateShare(_ sender: Any) {
+    }
+    
+    @IBAction func btnShareOpenFiles(_ sender: Any) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
