9c79a5f51e64fe120d638747b09e72cb3c848cce
Daniel Burgner <dburgnerjr@yahoo.com>
2017-07-21 21:59:49 -0400

    Completed Build Flavor Setup
    Completed build flavor setup for free and paid versions.

--- FlipulatorIOS/FlipulatorIOS/ViewController.swift
+++ FlipulatorIOS/FlipulatorIOS/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let paid = Bundle.main.object(forInfoDictionaryKey: "Paid")
+        if (paid != nil) {
+            label.text = "Flipulator Premium Version"
+        } else {
+            return
+        }
     }
 
     override func didReceiveMemoryWarning() {
