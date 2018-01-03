c53139b5ac00652dad59f9f66c591898345a5ad1
Daniel Burgner <dburgnerjr@yahoo.com>
2018-01-02 22:06:32 -0600

    Calculate Screen
    Continued Calculate Screen buildout.  Added ScrollView. #100DaysOfCode

---
+++ FlipulatorIOS/CalculateScreen.swift
@@ -0,0 +1,14 @@
+//
+//  CalculateScreen.swift
+//  FlipulatorIOS
+//
+//  Created by Daniel Burgner on 1/2/18.
+//  Copyright © 2018 Daniel Burgner. All rights reserved.
+//
+
+import UIKit
+
+class CalculateScreen: UIViewController {
+
+    
+}
