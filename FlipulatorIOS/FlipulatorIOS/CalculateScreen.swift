9a1fc0496bc60b1f05fc9d156e5995e81cea54b6
Daniel Burgner <dburgnerjr@yahoo.com>
2018-01-04 22:26:24 -0600

    CalculateScreen D4.
    Took one step forward, several steps back.  #100DaysOfCode

--- FlipulatorIOS/FlipulatorIOS/CalculateScreen.swift
+++ FlipulatorIOS/FlipulatorIOS/CalculateScreen.swift
@@ -2,7 +2,7 @@
 //  CalculateScreen.swift
 //  FlipulatorIOS
 //
-//  Created by Daniel Burgner on 1/2/18.
+//  Created by Daniel Burgner on 1/4/18.
 //  Copyright © 2018 Daniel Burgner. All rights reserved.
 //
 
@@ -12,15 +12,14 @@ class CalculateScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+
         let paid = Bundle.main.object(forInfoDictionaryKey: "Paid")
         if (paid != nil) {
         } else {
             return
         }
     }
-    
+
     @IBAction func helpButton(_ sender: Any) {
         let alertController = UIAlertController(title: "Help", message:
             "Enter the address and square footage of the property, including the number of bedrooms and bathrooms, sale price, ARV, budget items and rehab budget. Rehab budget can be a flat rate or a rehab type. Rehab types are classified as:  Low ($15/sf, yard work and painting), Medium ($20/sf > 1500 sf or $25/sf < 1500 sf, Low + kitchen and bathrooms, High ($30/sf, Medium + new roof), Super-High ($40/sf, complete gut job), Bulldozer ($125/sf, demolition and rebuild).", preferredStyle: UIAlertControllerStyle.alert)
@@ -28,9 +27,12 @@ class CalculateScreen: UIViewController {
             UIAlertActionStyle.default, handler:nil))
     }
     
-    @IBAction func submitButton(_ sender: Any) {let alertController = UIAlertController(title: "Submit", message:
-        "Sending info to be calculated.", preferredStyle: UIAlertControllerStyle.alert)
+    @IBAction func submitButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Submit", message:
+            "Sending info to be calculated.", preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction(title: "OK", style:
             UIAlertActionStyle.default, handler:nil))
     }
+    
+    
 }
