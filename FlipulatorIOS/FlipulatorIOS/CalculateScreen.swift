fde47ba260635afb286f37399b31ee807f8b0654
Daniel Burgner <dburgnerjr@yahoo.com>
2018-01-06 17:54:18 -0600

    Calculate Screen D6.
    Finished buildout of the Calculate Screen.  Encountered issue with
    Submit button (trying to present Results Screen when an alert
    controller is already presented). #100DaysOfCode

--- FlipulatorIOS/FlipulatorIOS/CalculateScreen.swift
+++ FlipulatorIOS/FlipulatorIOS/CalculateScreen.swift
@@ -12,6 +12,7 @@ class CalculateScreen: UIViewController {
     
     @IBOutlet weak var btnHelp: UIButton!
     @IBOutlet weak var btnSubmit: UIButton!
+    var helpAlertController: UIAlertController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,19 +23,20 @@ class CalculateScreen: UIViewController {
             return
         }
     }
-
+    
     @IBAction func btnHelp(_ sender: Any) {
-        let alertController = UIAlertController(title: "Help", message:
+        helpAlertController = UIAlertController(title: "Help", message:
             "Enter the address and square footage of the property, including the number of bedrooms and bathrooms, sale price, ARV, budget items and rehab budget. Rehab budget can be a flat rate or a rehab type. Rehab types are classified as:  Low ($15/sf, yard work and painting), Medium ($20/sf > 1500 sf or $25/sf < 1500 sf, Low + kitchen and bathrooms, High ($30/sf, Medium + new roof), Super-High ($40/sf, complete gut job), Bulldozer ($125/sf, demolition and rebuild).", preferredStyle: UIAlertControllerStyle.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style:
-            UIAlertActionStyle.default, handler:nil))
+        helpAlertController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertActionStyle.default, handler: nil))
+        present(helpAlertController, animated: true, completion: nil)
     }
     
     @IBAction func btnSubmit(_ sender: Any) {
-        let alertController = UIAlertController(title: "Submit", message:
-            "Sending info to be calculated.", preferredStyle: UIAlertControllerStyle.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style:
-            UIAlertActionStyle.default, handler:nil))
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "ResultsVC") as! ResultsScreen
+        self.present(nextViewController, animated: true, completion: nil)
     }
     
 }
---
+++ FlipulatorIOS/FlipulatorIOS/ResultsScreen.swift
@@ -0,0 +1,13 @@
+//
+//  ResultsScreen.swift
+//  FlipulatorIOS
+//
+//  Created by Daniel Burgner on 1/6/18.
+//  Copyright © 2018 Daniel Burgner. All rights reserved.
+//
+
+import UIKit
+
+class ResultsScreen: UIViewController {
+
+}
